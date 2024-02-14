9369c99bdeba65583a11affc4b4e101826ccf3f9
peterb3 <pbattisti20@gmail.com>
2024-02-14 14:54:49 -0800

    Scraper with working JSON scrape file

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ data = f.read()
 f.close()
 elements_to_scrape = json.loads(data)
 
-s = Scrape("AAPL", elements_to_scrape)
-print(s.summary())
+stock_list = ["AAPL", "TSLA", "GOOGL", "NFLX", "NVDA"]
+compiled_dict = {}
 
-# with open("test.json", "w") as outfile:
-#     json.dump(elements_to_scrape, outfile)
+for stock in stock_list:
+    stock_info = Scrape(stock, elements_to_scrape)
+    compiled_dict[stock] = stock_info.summary()
+
+with open("test.json", "w") as outfile:
+    json.dump(compiled_dict, outfile)
