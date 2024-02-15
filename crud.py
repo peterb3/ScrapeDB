c953a4651ff57e697067fa46452a2198b35cf02d
peterb3 <pbattisti20@gmail.com>
2024-02-14 16:30:22 -0800

    Scraper with working JSON scrape file

--- crud.py
+++ crud.py
@@ -1,13 +1,31 @@
 from sqlalchemy import create_engine
 from config import DATABASE_URI
 from models import Base, Stock
-# from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker
+import psycopg2
+import json
 
 engine = create_engine(DATABASE_URI)
 
-# Session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine)
 
 
-# def recreate_database():
-#     Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+def recreate_database():
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+
+s = Session()
+
+connection = psycopg2.connect(DATABASE_URI)
+cursor = connection.cursor()
+
+with open("test.json", "r") as infile:
+    data = json.load(infile)
+
+query_sql = """insert into stocks select * from json_populate_recordset (NULL::stocks, %s):"""
+cursor.execute(query_sql, (json.dumps(data),))
+
+connection.commit()
+connection.close()
+s.close()
